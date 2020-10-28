feabcffde92b41ad83c095c9e0621fd0c1c467cc
Oleksandr Prystay <72867129+JapkoLite@users.noreply.github.com>
2020-10-28 15:27:27 +0200

    Add files via upload

---
+++ dz/script.js
@@ -0,0 +1,226 @@
+// let hello = "hello"
+// let owu = "owu"
+// let com ="com"
+// let ua ="ua"
+// let number1 ="1"
+// let number2 ="10"
+// let number3 ="-999"
+// let number4 = "123"
+// let number5 ="3.14 "
+// let number6 ="2.7"
+// let number7 ="16"
+// let bool = true
+// let bool2= false
+// console.log(hello,owu,com,ua,number1,number2,number3,number4,number5,number6,number7,bool,bool2)
+// document.write(hello,owu,com,ua,number1,number2,number3,number4,number5,number6,number7,bool,bool2);
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(number4);
+// alert(number5);
+// alert(number6);
+// alert(number7);
+// alert(bool);
+// alert(bool2) ;
+
+//2//
+
+// let hello1 = "hello"
+// let owu1 = "owu"
+// let com1 ="com"
+// let ua1 ="ua"
+// let number11 ="1"
+// let number22 ="10"
+// let number33 ="-999"
+// let number44 = "123"
+// let number55 ="3.14 "
+// let number66 ="2.7"
+// let number77 ="16"
+// let bool11 = true
+// let bool22= false
+//
+// console.log(hello1,owu1,com1,ua1,number11,number22,number33,number44,number55,number66,number77,bool11,bool22)
+// document.write(hello1,owu1,com1,ua1,number11,number22,number33,number44,number55,number66,number77,bool11,bool22);
+// alert(hello1);
+// alert(owu1);
+// alert(com1);
+// alert(ua1);
+// alert(number11);
+// alert(number22);
+// alert(number33);
+// alert(number44);
+// alert(number55);
+// alert(number66);
+// alert(number77);
+// alert(bool11);
+// alert(bool22);
+
+// 3
+
+// const number1 = 10;
+// const number2 = 20;
+// const number3 = 30;
+// const string1 ="ten"
+// const string2 ="twenty"
+// const string3 ="thirty"
+//
+// document.write(number1,number2,number3,string1,string2,string3);
+// console.log(number1,number2,number3,string1,string2,string3);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert("ten");
+// alert("twenty");
+// alert("thirty");
+
+// 4
+
+// let pr = prompt("Прізвище")
+// let name = prompt("Ваше імя")
+// let fd = prompt("По батькові")
+// document.write(pr);
+// document.write(name);
+// document.write(fd);
+// console.log(pr,name,fd);
+// alert(pr);
+// alert(name);
+// alert(fd)
+
+// 5
+
+// let pr = prompt("Прізвище");
+// let name = prompt("Ваше імя");
+// let fd = prompt("По батькові");
+// let person = (pr+name+fd);
+// console.log(person);
+
+// 6
+
+// let pr = prompt("Прізвище");
+// let name = prompt("Ваше імя");
+// let fd = prompt("По батькові");
+// let person = (pr+name+fd);
+// console.log(person);
+// let pr1 = prompt("Прізвище");
+// let name1 = prompt("Ваше імя");
+// let fd1 = prompt("По батькові");
+// let person1 = (pr1+name1+fd1);
+// console.log(person1);
+// let pr2 = prompt("Прізвище");
+// let name2 = prompt("Ваше імя");
+// let fd2 = prompt("По батькові");
+// let person2 = (pr2+name2+fd2);
+// console.log(person2);
+
+//7
+
+// let number1 = prompt("number 1");
+// let number2 = prompt("number 2");
+// let number3 = prompt("number 3");
+// console.log(number1+number2+number3);
+
+//8
+
+// let number1 = parseInt(prompt("number 1"));
+// let number2 = parseInt(prompt("number 2"));
+// let number3 = parseInt(prompt("number 3"));
+// let number4 = parseInt(prompt("number 4"));
+// let result = number1+number2+number3+number4 ;
+// console.log(result);
+
+//9
+
+// let number1 = parseFloat(prompt("number 1"));
+// let number2 = parseFloat(prompt("number 2"));
+// let number3 = parseFloat(prompt("number 3"));
+// let result  = number1+number2+number3;
+// console.log(result);
+
+//10
+
+// let number1 = Math.round( prompt("number 1"));
+// let number2 = Math.round( prompt("number 2"));
+// let number3 = Math.round( prompt("number 3"));
+// let result  = number1+number2+number3;
+// console.log(result);
+
+//11
+
+// let number1 = parseInt(prompt("number 1"));
+// let number2 = parseInt(prompt("number 2"));
+// let result = Math.pow(number1,number2);
+// console.log(result);
+
+//12
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof a, typeof b, typeof c, typeof d);
+
+
+// 13
+let inequaliti= (5<6);
+if ( inequaliti){
+    console.log("True");
+}
+else console.log("False");
+inequaliti=(5>6);
+if ( inequaliti){
+    console.log("True");
+}
+else {console.log("False");}
+inequaliti=(5==6);
+if (inequaliti){
+    console.log("True");
+}
+else { console.log("False");}
+inequaliti=5>=6;
+if (inequaliti){
+    console.log("True");
+}
+else { console.log("False");}
+// 10
+let inequaliti2=(10==10);
+if (inequaliti2){
+    console.log("True");
+}
+else { console.log("False");}
+inequaliti2=(10===10);
+if (inequaliti2){
+    console.log("True");
+}
+else { console.log("False");}
+// 10
+let inequaliti3=(10!==10);
+if (inequaliti3){
+    console.log("True");
+}
+else { console.log("False");}
+inequaliti3=(10!=10);
+if (inequaliti3){
+    console.log("True");
+}
+else { console.log("False");}
+inequaliti3=(10>10);
+if (inequaliti3){
+    console.log("True");
+}
+else { console.log("False");}
+//
+let inequaliti4=(123==="123");
+if (inequaliti4){
+    console.log("True");
+}
+else { console.log("False");}
+inequaliti4=(123!=="123");
+if (inequaliti4){
+    console.log("True");
+}
+else { console.log("False");}
+
